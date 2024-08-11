89002b2a7537688a8f6903583a9485428d480307
gogi999 <g.petrovic9@gmail.com>
2024-08-11 11:22:13 +0200

    How to test a button with the component

--- cy-10/cypress/component/Stepper.cy.jsx
+++ cy-10/cypress/component/Stepper.cy.jsx
@@ -21,4 +21,31 @@ describe('<Stepper />', () => {
     // Assert
     cy.get(stepperSelector).should("contain.text", 100);
   });
+
+  it("Can be incremented", () => {
+    // Arrange
+    cy.mount(<Stepper />);
+    // Act
+    cy.get(incrementSelector).click()
+    // Assert
+    cy.get(stepperSelector).should("contain.text", 1);
+  });
+
+  it("Can be decremented", () => {
+    // Arrange
+    cy.mount(<Stepper />);
+    // Act
+    cy.get(decrementSelector).click()
+    // Assert
+    cy.get(stepperSelector).should("contain.text", -1)
+  });
+
+  it("Has an initial counter that can be incremented and decremented", () => {
+    cy.mount(<Stepper initial={100} />);
+    cy.get(stepperSelector).should("contain.text", 100)
+    cy.get(incrementSelector).click()
+    cy.get(stepperSelector).should("contain.text", 101)
+    cy.get(decrementSelector).click().click()
+    cy.get(stepperSelector).should("contain.text", 99)
+  });
 })
