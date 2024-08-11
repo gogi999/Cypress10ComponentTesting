9f0be8f289422e411ea2c7d5659e23ae09de4620
gogi999 <g.petrovic9@gmail.com>
2024-08-11 11:07:17 +0200

    Mounting a component and finished first two tests

--- cy-10/cypress/component/Stepper.cy.jsx
+++ cy-10/cypress/component/Stepper.cy.jsx
@@ -1,8 +1,24 @@
 /// <reference types="cypress" />
+
 import Stepper from './Stepper'
 
-describe('Steper.cy.jsx', () => {
-  it('playground', () => {
-    cy.mount(<Stepper />) // This command now works in any test
-  })
+describe('<Stepper />', () => {
+  // Set up some constants for the selectors
+  const stepperSelector = "[data-testid=stepper]";
+  const incrementSelector = "[aria-label=increment]";
+  const decrementSelector = "[aria-label=decrement]";
+
+  it("Stepper should default to 0", () => {
+    // Arrange
+    cy.mount(<Stepper />);
+    // Assert
+    cy.get(stepperSelector).should("contain.text", 0);
+  });
+
+  it('Supports an "initial" prop to set the value', () => {
+    // Arrange
+    cy.mount(<Stepper initial={100} />);
+    // Assert
+    cy.get(stepperSelector).should("contain.text", 100);
+  });
 })
--- cy-10/cypress/component/Stepper.jsx
+++ cy-10/cypress/component/Stepper.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 
+// eslint-disable-next-line react/prop-types
 export default function Stepper({ initial = 0 }) {
     const [count, setCount] = useState(initial)
 
     return (
         <div data-testid="stepper">
-            <button aria-label="decrement" onClick={decrement}>
+            <button aria-label="decrement" onClick={() => setCount(count - 1)}>
                 -
             </button>
             {count}
-            <button aria-label="increment" onClick={increment}>
+            <button aria-label="increment" onClick={() => setCount(count + 1)}>
                 +
             </button>
         </div>
